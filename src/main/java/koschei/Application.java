1bb59a646280a2a5adad50e69dcd522bdc26cb1c
SerjBulaffin <bulavins@bk.ru>
2021-11-07 14:04:35 +0700

    PP 2.1.4

---
+++ src/main/java/koschei/Application.java
@@ -0,0 +1,19 @@
+package koschei;
+
+import koschei.config.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        KoscheiTheDeathless koscheiTheDeathless =
+                applicationContext.getBean(KoscheiTheDeathless.class);
+        System.out.println(koscheiTheDeathless.getRulesByDeth());
+    }
+}
+/*
+«На море на океане есть остров, на том острове дуб стоит, под дубом сундук зарыт, в сундуке — заяц,
+в зайце — утка, в утке — яйцо, в яйце — игла, — смерть Кощея»
+ */
